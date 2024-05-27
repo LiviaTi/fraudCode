114413ab9f3e97bc4c8c6286f2a22acd8b89f6fe
liviaTI <liviasantferreira@gmail.com>
2024-05-27 10:23:23 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+    The goal is to find fraudulent orders as early as possible and flag them for deeper analysis.
+    Orders starting with the letter "B" encounter fraud at a rate 25 times the normal rate.
+*/
+string[] code = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+int count=0;
+
+foreach(string item in code){
+    count++;
+
+    if (item.StartsWith("B"))
+    {
+        Console.WriteLine($"The item {count} with the code {item} contains fraud");
+    }
+
+}
